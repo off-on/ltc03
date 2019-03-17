f74d2812afb2544bae24b5fbbd2377d81c465ea1
mikle <you@example.com>
2019-03-17 16:47:18 +0300

    c was unnecessary

--- ltc03.c/sumoftwo.c
+++ ltc03.c/sumoftwo.c
@@ -9,8 +9,8 @@ int main(){
   scanf("%d",&a);
   printf("Enter the second integer number to summarize and press Enter key: ");
   scanf("%d",&b);
-  c = a + b;
-  printf("The Summary is: %d",c);
+  c = a + b; // it works with spaces
+  printf("The Summary is: %d",a+b); // it works w/o spaces and w/o c too
   printf("\n\n\n");
   return 0;
 }
