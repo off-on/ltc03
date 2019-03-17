ba87700063f66b06cc022380abba0cc5718cf07f
mikle <you@example.com>
2019-03-17 22:57:52 +0300

    sum and print now ina separate function

--- ltc03.c/sumoftwo.c
+++ ltc03.c/sumoftwo.c
@@ -1,16 +1,18 @@
 #include "stdio.h"
 
+int printsum(int a, int b){
+  printf("The Summary is: %d",a+b);
+  printf("\n\n\n");
+}
+
 int main(){
   printf("\n\n This program will summarize two intergers given by the user.\n");
   int a;
   int b;
-  int c;
   printf("Enter the firt integer number to summarize, then press Enter key: ");
   scanf("%d",&a);
   printf("Enter the second integer number to summarize and press Enter key: ");
   scanf("%d",&b);
-  c = a + b; // it works with spaces
-  printf("The Summary is: %d",a+b); // it works w/o spaces and w/o c too
-  printf("\n\n\n");
+  printsum(a,b);
   return 0;
 }
